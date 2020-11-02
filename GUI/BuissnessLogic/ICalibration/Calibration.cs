5a8bd4b2d87a309b6027cdcbc0f99f5f74308a53
anekatdj <60734487+anekatdj@users.noreply.github.com>
2020-11-02 09:59:40 +0100

    Cal

--- GUI/BuissnessLogic/ICalibration/Calibration.cs
+++ GUI/BuissnessLogic/ICalibration/Calibration.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.Generic;
+using DTO;
+using DataAccessLogic;
 
 namespace BuissnessLogic
 {
     class Calibration : ICalibration
     {
         private double r2Val;
+        
+
         public List<DTO_CalVal> GetCalVal()
         {
             throw new System.NotImplementedException();
--- GUI/BuissnessLogic/ICalibration/ICalibration.cs
+++ GUI/BuissnessLogic/ICalibration/ICalibration.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using DTO;
+using DataAccessLogic;
 
 namespace BuissnessLogic
 {
