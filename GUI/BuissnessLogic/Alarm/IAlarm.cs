4548fc0622559fd55c2decdb05c81b05207c6589
Simon Mølgaard Gertsen <au558684@uni.au.dk>
2020-11-02 10:12:45 +0100

    Alarm fikset

--- GUI/BuissnessLogic/Alarm/IAlarm.cs
+++ GUI/BuissnessLogic/Alarm/IAlarm.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.Generic;
+using DTO;
 
 namespace BuissnessLogic
 {
     public interface IAlarm
     {
-        public List<DTO_Alarm> getAlarm() {}
+        public List<DTO_Measurement> AlarmData()
+        {
+            return AlarmData();
+        }
         public void StartAlarm(){}
         public void MuteAlarm(){}
     }
--- GUI/DTO/DTO_Measurement.cs
+++ GUI/DTO/DTO_Measurement.cs
@@ -9,18 +9,18 @@ namespace DTO
        public DateTime Date { get; set; }
        public List<int> SysData { get; set; }
        public List<int> DiaData { get; set; }
-       public List<int> Alarm { get; set; }
+       public List<int> AlarmData { get; set; }
        public List<int> Pulse { get; set; }
        public List<int> PowerData { get; set; }
 
        public DTO_Measurement(List<double> rawData, DateTime date, List<int> sysData, List<int> diaData,
-           List<int> alarm, List<int> pulse, List<int> powerData)
+           List<int> alarmData, List<int> pulse, List<int> powerData)
        {
            RawData = rawData;
            Date = date;
            SysData = sysData;
            DiaData = diaData;
-           Alarm = alarm;
+           AlarmData = alarmData;
            Pulse = pulse;
            PowerData = powerData;
        }
