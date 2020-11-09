ade6a8149f6c92aa9b9dcb0b9b0ecf6c3bec4242
Simon Mølgaard Gertsen <au558684@uni.au.dk>
2020-11-09 10:38:02 +0100

    Ha

--- GUI/DTO/DTO_CalVal.cs
+++ GUI/DTO/DTO_CalVal.cs
@@ -11,14 +11,16 @@ namespace DTO
         public double R2 { get; set; }
         public double A { get; set; } //slope
         public int B { get; set; } //y-intercept
+        public double Zv { get; set; }
 
-        public DTO_CalVal(List<int> calReference, List<double> calMeasured, double r2,double a, int b)
+        public DTO_CalVal(List<int> calReference, List<double> calMeasured, double r2,double a, int b, double zv)
         {
             CalReference = calReference;
             CalMeasured = calMeasured;
             R2 = r2;
             A = a;
             B = b;
+            Zv = zv;
         }
     }
 }
