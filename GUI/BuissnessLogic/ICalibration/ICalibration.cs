6d5833111e9f68b612871ea5e7879c1d346bc7ed
anekatdj <60734487+anekatdj@users.noreply.github.com>
2020-11-22 16:59:45 +0100

    r2

--- GUI/BuissnessLogic/ICalibration/ICalibration.cs
+++ GUI/BuissnessLogic/ICalibration/ICalibration.cs
@@ -8,7 +8,7 @@ namespace BuissnessLogic
     {
         public List<DTO_CalVal> GetCalVal();
 
-        public double CalculateR2Val(List<int> calReference, List<double> calMeasured);
+        public double CalculateR2Val(List<int> calReference, List<double> calMeasured,double r2);
 
         public List<DTO_CalVal> SaveCalval(List<int> calReference, List<double> calMeasured, double r2, double a, int b, int zv, string socSecNB);
 
