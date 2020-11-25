0dcaee92af96db7758b10c2ed03bdf45bbcc8284
anekatdj <60734487+anekatdj@users.noreply.github.com>
2020-11-25 14:16:02 +0100

    ReadFromFile i Datalag

---
+++ GUI/DataAccessLogic/ReadFromFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using DTO;
+
+namespace DataAccessLogic
+{
+    public class ReadFromFile
+    {
+        private List<DTO_Measurement> samples;
+        public List<DTO_Measurement> Measurrement()
+        {
+            samples = new List<DTO_Measurement>();
+
+            string[] lines = System.IO.File.ReadAllLines(@"..\..\..\Sample.txt");
+
+            foreach (string line in lines)
+            {
+                string[] splitline = line.Split(',');
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    samples[i].mmHg = Convert.ToDouble(splitline[0]);
+                    samples[i].Tid = Convert.ToDateTime(splitline[1]);
+                }
+            }
+
+            return samples;
+        }
+    }
+}
