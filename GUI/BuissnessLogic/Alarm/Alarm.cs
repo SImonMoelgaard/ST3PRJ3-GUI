6469ceb20f8dc8d4bda816aeefb8e7ffef6e2627
anekatdj <60734487+anekatdj@users.noreply.github.com>
2020-12-02 17:13:07 +0100

    alarm

--- GUI/BuissnessLogic/Alarm/Alarm.cs
+++ GUI/BuissnessLogic/Alarm/Alarm.cs
@@ -24,7 +24,7 @@ namespace BuissnessLogic
                     
                 }
             }
-            
+            //
             
             //alarmList.Add(receiveRPi.ReceiveMeasurment());
             return alarmList;
