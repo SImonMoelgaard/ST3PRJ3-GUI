835795a775e48ba45c3b67ea138725cddb26fd39
anekatdj <60734487+anekatdj@users.noreply.github.com>
2020-12-04 20:34:36 +0100

    Rettelser

--- GUI/PresentationLogic/Windows/DataWindow.xaml.cs
+++ GUI/PresentationLogic/Windows/DataWindow.xaml.cs
@@ -48,15 +48,10 @@ namespace PresentationLogic.Windows
                 Convert.ToInt32(diaULimit_TB.Text), Convert.ToInt32(diaLLimit_TB.Text), Convert.ToInt32(meanLLimit_TB.Text), Convert.ToInt32(meanULimit_TB.Text), Convert.ToString(socSecNb_TB.Text
                     ), 0 ,0);
 
-
             measurementWindow = new MeasurementWindow(controller, mainWindow, datawindow);
-
-
-
 
             this.Hide();
             
-
             measurementWindow.Show();
         }
     }
