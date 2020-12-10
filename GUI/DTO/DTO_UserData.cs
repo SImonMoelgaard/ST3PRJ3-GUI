4bc4517a05a6a39113fbe993069f99c3b2352ff6
anekatdj <60734487+anekatdj@users.noreply.github.com>
2020-12-10 16:49:16 +0100

    commts

--- GUI/DTO/DTO_UserData.cs
+++ GUI/DTO/DTO_UserData.cs
@@ -8,10 +8,21 @@ namespace DTO
     
     public class DTO_UserData 
     {
-
+        /// <summary>
+        /// User name
+        /// </summary>
         public string Username { get; set; }
+
+        /// <summary>
+        /// Password
+        /// </summary>
         public string Password { get; set; }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
         public DTO_UserData(string username, string password)
         {
             Username = username;
