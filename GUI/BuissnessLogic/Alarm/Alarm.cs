ae7cafd74405fcbbdaebd3dfc9946227707a7122
Simon Mølgaard Gertsen <au558684@uni.au.dk>
2020-12-10 14:31:08 +0100

    PUSH

--- GUI/BuissnessLogic/Alarm/Alarm.cs
+++ GUI/BuissnessLogic/Alarm/Alarm.cs
@@ -19,51 +19,11 @@ namespace BuissnessLogic
             _dataQueue = dataQueue;
         }
 
-        public void Run()
-        {
-            List<DTO_Measurement> alarms = new List<DTO_Measurement>();
-            while (true)
-            {
-                alarms = receiveRPi.ReceiveMeasurment();
-                bool high = false;
-                foreach (var alarm in alarms)
-                {
-                    high = alarm.HighSys;
-                }
-
-                DataContainer reading = new DataContainer();
-
-                reading.SetHighSys(high);
-                _dataQueue.Add(reading);
-                Thread.Sleep(10);
-            }
-            _dataQueue.CompleteAdding();
-        }
-        //Producer slut
 
         public List<DTO_Measurement> AlarmData()
         {
-            List<DTO_Measurement> alarmList = new List<DTO_Measurement>();
-            List<bool> alarms = new List<bool>();
-
-            while (true)
-            {
-                alarmList = receiveRPi.ReceiveMeasurment();
-
-                foreach (var alarm in alarmList)
-                {
-                    alarms.Add(alarm.HighDia);
-                    
-                }
-            }
-            //
-            
-            //alarmList.Add(receiveRPi.ReceiveMeasurment());
-            return alarmList;
+            throw new System.NotImplementedException();
         }
-
-
-      
 
         public void StartAlarm()
         {
